7ae7753baa5bb4ce33dd0ef3e21325a85b4bfb5a
EC2 Default User <ec2-user@ip-172-31-46-206.ec2.internal>
2022-04-17 12:29:44 +0000

    [Add]User info,一覧ページ(本),詳細ページ(本)の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,9 +12,18 @@ class BooksController < ApplicationController
 
 
   def index
+    @books = Book.all
+
   end
 
   def show
+     @book = Book.find(params[:id])
+  end
+
+  def destroy
+   book = Book.find(params[:id])
+   book.destroy
+   redirect_to books_path
   end
 
   private
