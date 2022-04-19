ec86b2208ca5e6b2809f9a2d203533120c7c9c4f
EC2 Default User <ec2-user@ip-172-31-46-206.ec2.internal>
2022-04-19 02:47:41 +0000

    [Update]レイアウト(Top,about)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
   devise_for :users
+  root to: 'homes#top'
+  get "homes/about" => "homes#about", as: "about"
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:show, :edit, :update, :index]
 
